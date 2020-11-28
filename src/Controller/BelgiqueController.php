8fac877c1cbf0d1a6b2460b086339f601d8c0b70
Robert Faufron <robert.faufron@free.fr>
2020-11-28 19:12:04 +0100

    ajout 4

--- src/Controller/BelgiqueController.php
+++ src/Controller/BelgiqueController.php
@@ -5,16 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
 
 class BelgiqueController extends AbstractController
 {
     /**
      * @Route("/belgique", name="belgique")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('belgique/index.html.twig', [
-            'controller_name' => 'BelgiqueController',
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
